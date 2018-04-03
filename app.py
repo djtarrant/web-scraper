cc728f5806a5668d9ac693df5be17d5755e5a817
Darren <Darren>
2018-04-03 07:52:00 +0100

    Modified app.py to run new json function and open html file

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from engine import  test_url, url_scrape, frequency_distribution, lexical_diversity
+from engine import  test_url, url_scrape, frequency_distribution, lexical_diversity, generate_json
+import webbrowser, os
 
-
+filename = 'wordcloud.html'
 url = input('Please enter the URL - ')
 if(len(url) == 0):
-    url = 'http://www.darrentarrant.co.uk/myportfolio.php'
+    url = 'http://www.bbc.co.uk'
 
 try:
     test_url(url)
@@ -16,3 +17,7 @@ text = url_scrape(url)
 print(text)
 print("Lexical diversity:",lexical_diversity(text))
 print("Frequency Distribution:",frequency_distribution(text, 20))
+#generate our JSON for the wordcloud
+generate_json(text)#optionally add the font sizes
+print("Open",filename,"in the browser to see the wordcloud. Trying to open...")
+webbrowser.open('file://' + os.path.realpath(filename))
